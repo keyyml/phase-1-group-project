9eed2e016a83326b97ed839eb01a2c89f918fef1
Jordan Adams <jcadamsboomer@gmail.com>
2023-09-06 11:01:03 -0500

    same as main

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
 fetch("http://localhost:3000/mythological-monsters")
     .then((resp) => resp.json())
-    .then((data) => renderCreatureCards(data))
+    .then((data) => renderCreatures(data))
 
-function renderCreatureCards(creatureArr) {
-    const creatureCollection = document.querySelector('#list-creatures-here')
+    function renderCreatures(creaturesArr) {
 
-    // console.log(creatureArr)
-    // console.log(creatureCollection)
-    creatureArr.forEach((creatureObj) => {
-        const card = document.createElement('div')
-        card.className = ('card')
-        
-        const h2 = document.createElement('h2')
-        h2.textContent = creatureObj.name
-        card.appendChild(h2)
+        const ul = document.querySelector('#list-creatures-here')
 
-        const img = document.createElement('img')
-        img.src = creatureObj.image
-        img.alt = creatureObj.name 
-        card.appendChild(img)
+        creaturesArr.forEach((creaturesObj) => {
+            //console.log(creaturesObj)
 
-        const h3 = document.createElement('h3')
-        h3.textContent = creatureObj.origin
-        card.appendChild(h3)
-        
-        const h4 = document.createElement('h4')
-        h4.textContent = creatureObj.description
-        card.appendChild(h4)
-       
-        creatureCollection.appendChild(card)
-        
-        console.log(creatureCollection)
+            const creatureCards = document.createElement('li')
+            //console.log(li)
+            creatureCards.classList = "list-li"
+            creatureCards.textContent = creaturesObj.name
+            //console.log(creatureCards)
 
-    })
+            const img = document.createElement('img')
+            img.src = creaturesObj.image
+            img.alt = creaturesObj.name
+            
+            //console.log(img)
 
-}
+            creatureCards.appendChild(img)
+            //console.log(creatureCards)
+            ul.appendChild(creatureCards)
+            console.log(ul)
+             
+            const originInfo = document.createElement('h3')
+            originInfo.textContent = creaturesObj.origin
+            creatureCards.appendChild(originInfo)
+            
+            const descInfo = document.createElement('h4')
+            descInfo.textContent = creaturesObj.description
+            creatureCards.appendChild(descInfo)
+
+
+            
+        });
+    }
